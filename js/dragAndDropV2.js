3cf09ae6d622a1b5925c133864fde0a755278a17
Kremen <rostyslav.kremen@saxoprint.de>
2026-05-22 14:24:52 +0200

    animation für drag für computer

--- js/dragAndDropV2.js
+++ js/dragAndDropV2.js
@@ -21,7 +21,7 @@ const enableDragAndDrop = (li) => {
     // Für Handy
     li.addEventListener('touchstart', (e) => {
         dragSrcEl = li;
-        li.classList.add('dragging');
+        li.classList.add('draggingPhone');
     }, { passive: true });
 
 
@@ -30,26 +30,24 @@ const enableDragAndDrop = (li) => {
 
         // finden element unter finger
         const touch = e.touches[0];
-
-        // ховаємо елемент що тягнемо щоб знайти що під ним -- verstecken ziehende element dass was unter finden
-        dragSrcEl.style.display = 'none';
-        const elementBelow = document.elementFromPoint(touch.clientX, touch.clientY);
-        dragSrcEl.style.display = '';
+   
+        dragSrcEl.style.display = 'none'; // ховаємо елемент що тягнемо щоб знайти що під ним -- verstecken ziehende element dass was unter finden     // НАВІЩО? бо якщо він видимий — він сам буде // "під пальцем" і ми не побачимо що за ним
+        const elementBelow = document.elementFromPoint(touch.clientX, touch.clientY); // elementFromPoint — fragt browser: welche element sit in koordinate X, Y- browser schaut auf Display und gibt zurück elemnt unter finger
+        dragSrcEl.style.display = ''; // zurückgeben das Element 
 
         // when unter finger ist li - transportieren
         if (elementBelow) {
-            const targetLi = elementBelow.closest('li');
+            const targetLi = elementBelow.closest('li'); // closest('li') - geht oben in DOM und sucht in der hähe li -- sucht genau li element 
             const taskList = document.getElementById('task_list');
-
-            if (targetLi && targetLi !== dragSrcEl) {
-                const allLi = [...taskList.querySelectorAll('li')];
-                const dragIndex = allLi.indexOf(dragSrcEl);
-                const targetIndex = allLi.indexOf(targetLi);
+            if (targetLi && targetLi !== dragSrcEl) {  // tagetLi das ist nicht desellber li dass wir ziehen
+                const allLi = [...taskList.querySelectorAll('li')]; // massiv Li
+                const dragIndex = allLi.indexOf(dragSrcEl); // finden die zihende position 
+                const targetIndex = allLi.indexOf(targetLi); // finden die untere finger position 
 
                 if (dragIndex < targetIndex) {
-                    taskList.insertBefore(dragSrcEl, targetLi.nextSibling);
+                    taskList.insertBefore(dragSrcEl, targetLi.nextSibling); // unter ziehen stellen nach targetLi -- .nextSibling nächste elemnt nach targetli 
                 } else {
-                    taskList.insertBefore(dragSrcEl, targetLi);
+                    taskList.insertBefore(dragSrcEl, targetLi); // oben ziehen -> stellen vor targetLi
                 }
             }
         }
@@ -57,7 +55,7 @@ const enableDragAndDrop = (li) => {
 
 
     li.addEventListener('touchend', () => {
-        dragSrcEl.classList.remove('dragging');
+        dragSrcEl.classList.remove('draggingPhone');
         saveTaskLocalStorage();
     });
 };
